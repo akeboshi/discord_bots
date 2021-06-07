68a6f5df5a8a791a23397888b45b74a3b56be4b4
akeboshi <isao-aruga@bm-sms.co.jp>
2021-06-07 13:00:08 +0900

    add custom response

--- omikuji/omikuji_client.rb
+++ omikuji/omikuji_client.rb
@@ -4,6 +4,7 @@ require 'discordrb'
 require 'faraday'
 require 'json'
 bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN'], client_id: ENV['DISCORD_ID']
+api_host = ENV.fetch('API_HOST', 'http://localhost')
 
 def user_name(event)
   au = event.author
@@ -12,11 +13,20 @@ end
 
 bot.message(with_text: /(占い)|(uranai\d*)|(うらない)|(omikuji)|(おみくじ)/) do |event|
   user_id = event.author.id
-  con = Faraday.new("https://omikuji.akeboshi.dev", ssl: { verify: false })
+  con = Faraday.new(api_host, ssl: { verify: false })
   res = con.get("/api/omikujis/random/#{user_id}")
   body = JSON.parse(res.body)
   omikuji = body['name']
   event.respond "#{user_name(event)} は 【#{omikuji}】 です!"
+end
+
+bot.message do |event|
+  con = Faraday.new(api_host, ssl: { verify: false })
+  res = con.get('/api/custom_responses/words', {word: event.content})
+  words = JSON.parse(res.body.force_encoding('utf-8'))['words']
+  words.each do |w|
+    event.respond w
+  end
 end
 
 bot.run
